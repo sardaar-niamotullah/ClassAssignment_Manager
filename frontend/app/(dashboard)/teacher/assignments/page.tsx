0cd828fbf3165f89fb0ebffce684a56340cf99cd
sardaar-niamotullah <sardaar.niamotullah@gmail.com>
2026-08-13 12:32:59 +0600

    feat: introduces routes to teacher

---
+++ frontend/app/(dashboard)/teacher/assignments/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Teacher Assignments page
+      </h1>
+    </main>
+  );
+}
---
+++ frontend/app/(dashboard)/teacher/submissions/page.tsx
@@ -0,0 +1,9 @@
+export default function GuestPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Teacher submission page
+      </h1>
+    </main>
+  );
+}
