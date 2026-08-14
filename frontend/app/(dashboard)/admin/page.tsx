9b25f0e8c0adac69b5a783938a1eae81b565dfdb
sardaar-niamotullah <sardaar.niamotullah@gmail.com>
2026-08-14 08:02:08 +0600

    feat: add redirection to admin page

--- frontend/app/(dashboard)/admin/page.tsx
+++ frontend/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function AdminDashboard() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Admin Screen</h1>
-    </main>
-  );
+  redirect("admin/users");
 }
